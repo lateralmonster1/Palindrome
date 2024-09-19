f567af9fcfed0133dada93dac135ae68594bd420
Xavier <xavier3435@icloud.com>
2024-09-19 09:19:54 -0400

    split languages

--- index.js
+++ index.js
@@ -1,17 +1,69 @@
-const input = document.getElementById("input")
-
-function reverseString(str) {
-    return str.split("").reverse().join("")
-   
-}
 function check() {
-    const value = input.value
-    const reverse = reverseString(value)
-    
-if (value == reverse) {
-    alert("P A L I N D R O M E")
-} else {
-    alert("Not a Palindrome!")
+    const input = document.getElementById('input').value;
+    const cleanedInput = input.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
+    const isPalindrome = cleanedInput === cleanedInput.split('').reverse().join('');
+    alert(isPalindrome ? 'Palindrome!' : 'Not a palindrome.');
 }
-input.value = ""
+
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+const particles = [];
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+class Particle {
+    constructor() {
+        this.x = Math.random() * canvas.width;
+        this.y = Math.random() * canvas.height;
+        this.size = Math.random() * 5 + 1;
+        this.speedX = Math.random() * 3 - 1.5;
+        this.speedY = Math.random() * 3 - 1.5;
+        this.opacity = Math.random();
+    }
+
+    update() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+
+        if (this.x > canvas.width || this.x < 0) {
+            this.speedX = -this.speedX;
+        }
+
+        if (this.y > canvas.height || this.y < 0) {
+            this.speedY = -this.speedY;
+        }
+    }
+
+    draw() {
+        ctx.fillStyle = `rgba(0, 123, 255, ${this.opacity})`;
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+        ctx.fill();
+    }
 }
+
+function initParticles() {
+    for (let i = 0; i < 100; i++) {
+        particles.push(new Particle());
+    }
+}
+
+function animateParticles() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    particles.forEach(particle => {
+        particle.update();
+        particle.draw();
+    });
+
+    requestAnimationFrame(animateParticles);
+}
+
+initParticles();
+animateParticles();
+
+window.addEventListener('resize', () => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+});
